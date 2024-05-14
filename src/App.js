66b0c352e1683c9e5cba29aba3af792879d9b106
ayonssp-GohilTech <ayon.karmakar@gohiltech.com>
2024-05-14 14:56:09 +0530

    feat: added Toaster 🎶

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
 import PageTitle from './components/PageTitle';
 import AppHeader from './components/AppHeader.jsx';
 import styles from './styles/modules/app.module.scss';
+import Button from './components/Button';
+import { toast } from 'react-hot-toast';
 
 function App() {
   return (
@@ -12,6 +15,21 @@ function App() {
           <AppHeader />
         </div>
       </div>
+      {/* temp test */}
+      <Button onClick={() => { toast.success('Please enter a title'); }}>
+        Show Toast
+      </Button>
+      <Button variant='secondary' onClick={() => { toast.error('Please enter a title'); }}>
+        Show Toast
+      </Button>
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          style: {
+            fontSize: '1.4rem',
+          },
+        }}
+      />
     </>
   );
 }
