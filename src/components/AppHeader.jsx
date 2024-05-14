5de9400b540cd7aba1e9ecddb8e0e15c17e91890
ayonssp-GohilTech <ayon.karmakar@gohiltech.com>
2024-05-14 13:43:41 +0530

    feat(ui): Button

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 import styles from '../styles/modules/app.module.scss';
-
+import Button from './Button';
 
 const AppHeader = () => {
   return (
     <div className={styles.appHeader}>
-      AppHeader
+      <Button variant='primary'>
+        Show Toast
+      </Button>
+      <Button variant='secondary'>
+        Show Toast
+      </Button>
     </div>
   )
 }
