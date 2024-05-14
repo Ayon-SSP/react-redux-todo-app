14c00f91e02e8c520cbff589fd04f0effa3421ff
ayonssp-GohilTech <ayon.karmakar@gohiltech.com>
2024-05-14 13:27:58 +0530

    feat: added AppHeader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PageTitle from './components/PageTitle';
+import AppHeader from './components/AppHeader.jsx';
+import styles from './styles/modules/app.module.scss';
 
 function App() {
   return (
     <>
       <div className="container">
         <PageTitle>TODO List</PageTitle>
+        <div className={styles.app__wrapper}>
+          <AppHeader />
+        </div>
       </div>
     </>
   );
---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import styles from '../styles/modules/app.module.scss';
+
+
+const AppHeader = () => {
+  return (
+    <div className={styles.appHeader}>
+      AppHeader
+    </div>
+  )
+}
+
+export default AppHeader
