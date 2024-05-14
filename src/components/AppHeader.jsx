8a2ac36207e26406c91339341cb6952850c7093e
ayonssp-GohilTech <ayon.karmakar@gohiltech.com>
2024-05-14 13:54:39 +0530

    feat: added SelectButton

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import styles from '../styles/modules/app.module.scss';
 import Button from './Button';
+import SelectButton from './SelectButton';
 
 const AppHeader = () => {
   return (
@@ -11,6 +12,16 @@ const AppHeader = () => {
       <Button variant='secondary'>
         Show Toast
       </Button>
+
+      <SelectButton
+        id="status"
+      // onChange={(e) => updateFilter(e)}
+      // value={filterStatus}
+      >
+        <option value="all">All</option>
+        <option value="incomplete">Incomplete</option>
+        <option value="complete">Completed</option>
+      </SelectButton>
     </div>
   )
 }
---
+++ src/components/SelectButton.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from '../styles/modules/button.module.scss';
+import { getClasses } from '../utils/getClasses';
+
+const SelectButton = ({children, id, ...rest}) => {
+  return (
+    <select 
+      id={id}
+      className={getClasses([
+        styles.button,
+        styles.button__select,
+      ])}
+      {...rest}
+    >
+      {children}
+    </select>
+  )
+}
+
+export default SelectButton;
