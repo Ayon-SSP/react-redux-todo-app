bbff5d31bafab5a7fa1145d6476552f7e338bed0
ayon_ssp <ayon.ssp@gmail.com>
2024-05-19 18:08:22 +0530

    feat: AppContent

--- src/App.js
+++ src/App.js
@@ -4,26 +4,15 @@ import PageTitle from './components/PageTitle';
 import AppHeader from './components/AppHeader.jsx';
 import AppContent from './components/AppContent.jsx';
 import styles from './styles/modules/app.module.scss';
-import CheckButton from './components/CheckButton.jsx';
 
 function App() {
-
-  const [checked, setChecked] = useState(false);
-
-  const handleCheck = () => {
-    setChecked(!checked);
-    // dispatch(
-    //   updateTodo({ ...todo, status: checked ? 'incomplete' : 'complete' })
-    // );
-  };
-
   return (
     <>
       <div className="container">
         <PageTitle>TODO List</PageTitle>
         <div className={styles.app__wrapper}>
           <AppHeader />
-          <CheckButton checked={checked} handleCheck={handleCheck} />
+          <AppContent />
         </div>
       </div>
       <Toaster
